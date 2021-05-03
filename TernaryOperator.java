eaa4604be6f5ea330b6969626db6d6ef9303ef90
MJ Silverio <mark.julius.silverio@accenture.com>
2021-05-03 22:01:42 +0800

    TernaryOperator.java

---
+++ TernaryOperator.java
@@ -0,0 +1,11 @@
+public class TernaryOperator {
+    public static boolean isEqual(int num1, int num2) {
+		boolean result = (num1 == num2) ? true : false;
+		return result;
+    }
+
+    public static boolean lessThanOrEqualToZero(int num) {
+        boolean result = (num <= 0) ? true : false;
+            return result;
+    }
+}
